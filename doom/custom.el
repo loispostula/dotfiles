f9591e4966e95962ddd7da75dda08daa1ab2b4f3
Loïs Postula <lois@postu.la>
2023-04-26 11:19:55 +0200

    fix: bring correct doom config

--- doom/custom.el
+++ doom/custom.el
@@ -4,15 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((rustic-default-clippy-arguments . "--no-default-features --features cmaes-macos")
-     (rustic-default-test-arguments . "--no-default-features --features cmaes-macos -- --include-ignored")
+   '((rustic-default-clippy-arguments . "--benches --tests")
      (rustic-default-test-arguments . "--benches --tests")
-     (rustic-cargo-tests-arguments . "--benches --tests")
-     (rustic-cargo-check-arguments . "--benches")
-     (rustic-cargo-check-arguments . "--no-default-features")
-     (rustic-cargo-check-arguments . "--no-default-features --features cmaes-macos")
-     (rustic-cargo-check-arguments . "--tests --no-default-features --features cmaes-macos")))
- '(warning-suppress-types '((initialization) (initialization) (defvaralias)) t))
+     (rustic-cargo-check-arguments . "--benches"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
